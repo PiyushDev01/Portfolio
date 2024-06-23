a5a8ec51c4cc373d228e84b2421a96137e4c9149
Piyush Vishwakarma <piyushvishwakarma6706@gmail.com>
2024-06-24 01:27:32 +0530

    firstcommit

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -12,7 +12,7 @@ function Nav() {
 //   bg-[#11111b]
   return (
     <>
-      <div className="flex flex-col absolute items-center justify-center w-full h-[10rem]  z-10">
+      <div className="flex flex-col absolute items-center justify-center w-full md:h-[10rem] mt-5 md:mt-0  z-10">
         <div
           id="nav"
           className=  " bg-white/10 backdrop-blur-sm h-2/4 rounded-full w-4/5 flex flex-row items-center px-[1rem] justify-between"
@@ -20,7 +20,7 @@ function Nav() {
           <img 
             src={profile}
             id="profileimage"
-            className="bg-slate-500 w-[3.5rem] h-[3.5rem] rounded-full"
+            className="bg-slate-500 w-12 my-2 md:my-0 md:w-[3.5rem] md:h-[3.5rem] rounded-full"
           ></img>
           <ul className="hidden md:flex flex-row gap-16 px-4 font-fira text-slate-100 font-medium">
             <li>{"</Home>"}</li>
@@ -37,8 +37,8 @@ function Nav() {
         </div>
         <div
           id="mobnav"
-          style={{ height: toggle ? "320px" : "0px", borderWidth: toggle? "1px":"0px" }}
-          className=" overflow-hidden  flex items-center justify-center  transition-all -z-10 rounded-b-[3rem] md:hidden top-[5rem] absolute w-4/5 bg-white/5 backdrop-blur-sm"
+          style={{ height: toggle ? "320px" : "0px" }}
+          className=" overflow-hidden  flex items-center justify-center  transition-all -z-10 rounded-b-[3rem] md:hidden top-[2rem] absolute w-4/5 bg-white/5 backdrop-blur-sm"
         >
           <ul className="flex items-center justify-center gap-8 flex-col font-fira text-slate-50 text-lg ">
             <li>{"</Home>"}</li>
