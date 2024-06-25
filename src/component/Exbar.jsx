bb698704971069bbc3668a9f4e3c440fcb24d0f9
Piyush Vishwakarma <piyushvishwakarma6706@gmail.com>
2024-06-25 22:41:36 +0530

    debug

--- src/component/Exbar.jsx
+++ src/component/Exbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Exbar(props) {
 
-    const color = props.l==1?"from-green-800 to-green-500":props.l==2?"from-yellow-800 to-yellow-500":"from-red-800 to-red-500" ;
+    const color = props.l==1?" w-full from-green-800 to-green-500":props.l==2?" w-[50%] from-yellow-800 to-yellow-500":" w-[20%] from-red-800 to-red-500" ;
 
 
   return (
    <>
     <div className=' w-[80%] rounded-full  h-fit bg-[#11111b] my-4 '>
-        <div className={`w-1/${props.l} h-1 bg-gradient-to-r ${color} rounded-full`}
+        <div className={` h-1 bg-gradient-to-r ${color} rounded-full`}
  ></div>
     </div>
    </>
