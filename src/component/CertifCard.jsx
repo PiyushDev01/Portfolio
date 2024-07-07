1979bc4bb176e032fa2e70106b00baebb2e51277
Piyush Vishwakarma <80773079+PiyushDev01@users.noreply.github.com>
2024-07-07 18:15:43 +0530

    certificate fadeup remove

--- src/component/CertifCard.jsx
+++ src/component/CertifCard.jsx
@@ -11,7 +11,7 @@ function CertifCard(props) {
   return (
     <>
     <motion.div 
-    variants={fadeIn("up", 0.3)}
+    variants={fadeIn("", 0.3)}
     initial="hidden"
     whileInView={"show"}
     viewport={{ once: false, amount: 0.1 }}
