8a61c01ff6bc58db85440f8f2ef1371debe07b55
Piyush Vishwakarma <80773079+PiyushDev01@users.noreply.github.com>
2025-02-21 02:24:24 +0530

    Update Like.jsx

--- src/component/Like.jsx
+++ src/component/Like.jsx
@@ -8,7 +8,7 @@ import { database } from '../firebase/firebase.js';
 function Like() {
 
 
-    const [value, setValue] = useState('000');
+    const [value, setValue] = useState();
 
 
     const handleChange = (e) => {   
